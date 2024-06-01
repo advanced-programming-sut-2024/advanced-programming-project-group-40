0c514f2868d695bf3a9927945c4cec631aabe6da
arkadashsiz <applest.ariamoosavi@gmail.com>
2024-06-02 00:32:39 +0330

    Its working yipiiii

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import com.google.gson.Gson;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        //this is a test (send help)
+        Gson gson = new Gson();
+        JFrame jFrame = new JFrame();
+        System.out.println("they/them");
+    }
+}
