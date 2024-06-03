1b95a6df6f5ff01c94737c3eb9ad77f07c35526b
sarina <sarina.farzadnasab@gmail.com>
2024-06-03 22:02:30 +0330

    feat(sign up):sign up UI added

--- src/main/java/views/SignUpMenu.java
+++ src/main/java/views/SignUpMenu.java
@@ -2,9 +2,7 @@ package views;
 
 import javafx.stage.Stage;
 
-import java.util.Scanner;
-
-public class RegisterMenu extends PlayMenu{
+public class SignUpMenu extends PlayMenu{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
