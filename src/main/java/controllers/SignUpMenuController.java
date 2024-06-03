2bc547504a94d180b415052c478c8be61cd35251
sarina <sarina.farzadnasab@gmail.com>
2024-06-04 00:11:02 +0330

    feat(User info): user info validation and random pass generator added

--- src/main/java/controllers/SignUpMenuController.java
+++ src/main/java/controllers/SignUpMenuController.java
@@ -2,7 +2,7 @@ package controllers;
 
 import models.Result;
 
-public class RegisterMenuController extends UserInfoController {
+public class SignUpMenuController extends UserInfoController {
     public static Result register(String username, String password, String confirmPassword, String nickname, String email){
         return null;
     }
--- src/main/java/controllers/UserInfoController.java
+++ src/main/java/controllers/UserInfoController.java
@@ -1,26 +1,71 @@
 package controllers;
 
-public abstract class UserInfoController extends Controller{
+import models.Game;
+
+import java.security.SecureRandom;
+
+public abstract class UserInfoController extends Controller {
+
+    private static SecureRandom random = new SecureRandom();
+
     public static boolean isUserNameUnique(String username) {
-        return true;
+        return Game.getUserByName(username) == null;
     }
 
     public static boolean isUsernameValid(String username) {
-        return true;
+        return username.matches("[a-zA-Z0-9\\-]+");
     }
-    public static boolean isEmailValid(String email){
-        return true;
+
+    public static boolean isEmailValid(String email) {
+        return email.matches("\\S+@(gmail|yahoo|email).com");
     }
-    public static boolean isPasswordValid(String password){
-        return true;
+
+    public static boolean isPasswordValid(String password) {
+        return password.matches("[0-9a-zA-Z!@#$%^&*\\-]+");
     }
-    public static boolean isPasswordWeak(String password){
-        return true;
+
+    public static boolean isPasswordWeak(String password) {
+        return password.matches("^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[!@#$%^&*\\-]+)[0-9a-zA-Z!@#$%^&*\\-]+$") && password.length() > 7;
     }
-    public static boolean isPasswordTheSame(String password, String confirmPassword){
-        return true;
+
+    public static boolean isPasswordTheSame(String password, String confirmPassword) {
+        return password.equals(confirmPassword);
     }
-    public static String getRandomPassword(){
-        return null;
+
+    public static String getRandomPassword() {
+        return generatePassword();
     }
-}
+
+
+    private static String generatePassword() {
+        String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+        String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String DIGITS = "0123456789";
+        String SPECIAL_CHARS = "!@#$%^&*-";
+        String ALL_CHARS = LOWERCASE + UPPERCASE + DIGITS + SPECIAL_CHARS;
+        int PASSWORD_LENGTH = 8;
+
+
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+
+        // Ensure the password contains at least one of each required character type
+        password.append(LOWERCASE.charAt(random.nextInt(LOWERCASE.length())));
+        password.append(UPPERCASE.charAt(random.nextInt(UPPERCASE.length())));
+        password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        password.append(SPECIAL_CHARS.charAt(random.nextInt(SPECIAL_CHARS.length())));
+
+        // Fill the remaining characters randomly from the allowed characters
+        for (int i = 4; i < PASSWORD_LENGTH; i++) {
+            password.append(ALL_CHARS.charAt(random.nextInt(ALL_CHARS.length())));
+        }
+
+        // Shuffle the characters to ensure randomness
+        for (int i = 0; i < password.length(); i++) {
+            int randomIndex = random.nextInt(password.length());
+            char temp = password.charAt(i);
+            password.setCharAt(i, password.charAt(randomIndex));
+            password.setCharAt(randomIndex, temp);
+        }
+
+        return password.toString();
+    }
