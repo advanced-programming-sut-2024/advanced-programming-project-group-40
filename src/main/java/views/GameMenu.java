00e27e7c0f4ef6700670d4cbe9a9b20b38ccb1ac
arkadashsiz <applest.ariamoosavi@gmail.com>
2024-06-27 13:08:04 +0330

    a

--- src/main/java/views/GameMenu.java
+++ src/main/java/views/GameMenu.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.stage.Stage;
 import models.MatchTable;
 import models.UserInputHandler.ClickCommand;
 import models.cards.Card;
