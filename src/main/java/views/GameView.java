386de1fd5c06b578d289ee150d7e5f22239c996e
arkadashsiz <applest.ariamoosavi@gmail.com>
2024-06-27 22:13:28 +0330

    My name is Yoshikage Kira. I’m 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don’t smoke, but I occasionally drink. I’m in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning.
    I was told there were no issues at my last check-up. I’m trying to explain that I’m a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn’t lose to anyone.

--- src/main/java/views/GameView.java
+++ src/main/java/views/GameView.java
@@ -1,25 +1,12 @@
 package views;
 
 
-import enums.cards.UnitCardInfo;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import models.MatchTable;
-import models.UserInputHandler.ClickCommand;
-import models.cards.Card;
-import models.cards.UnitCard;
 
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
 public class GameView extends PlayMenu{
     public static void main(String[] args) {
