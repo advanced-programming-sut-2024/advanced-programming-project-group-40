0816b2147ee607da8531573ff0a72963fb4902e4
arkadashsiz <applest.ariamoosavi@gmail.com>
2024-07-01 01:17:07 +0330

    i hate git

--- src/main/java/views/ViewController/GameViewController.java
+++ src/main/java/views/ViewController/GameViewController.java
@@ -26,6 +26,7 @@ import javafx.stage.Stage;
 import models.Game;
 import models.MatchTable;
 import models.User;
+import models.UserInputHandler.CardClickCommand;
 import models.cards.Card;
 import models.cards.SpecialCard;
 import models.cards.UnitCard;
@@ -226,6 +227,9 @@ public class GameViewController extends PlayMenu implements Initializable {
         for (Card card : cards) {
             card.setOnMouseClicked(_ -> {
                 System.out.println(STR."name:\{card.getName()}");
+                CardClickCommand cardClickCommand = new CardClickCommand(card, this);
+                cardClickCommand.excute();
+
 
             });
         }
