68ca6ed3f9b670afbf722d58748ce9e34e161135
sarina <sarina.farzadnasab@gmail.com>
2024-07-01 22:22:05 +0330

    refactor(change faction): methods changed

--- src/main/java/views/ViewController/PreGameViewController.java
+++ src/main/java/views/ViewController/PreGameViewController.java
@@ -1,12 +1,6 @@
 package views.ViewController;
 
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
-import controllers.MenuController.PreGameMenuController;
-import enums.Factions;
-import enums.cards.UnitCardInfo;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
@@ -16,7 +10,6 @@ import javafx.scene.layout.VBox;
 import models.Game;
 import models.User;
 import models.cards.Card;
-import models.cards.UnitCard;
 import views.GameView;
 
 import java.util.ArrayList;
@@ -25,48 +18,44 @@ import java.util.Objects;
 
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import views.PreGameMenu;
 
 public class PreGameViewController {
-    public ImageView image2;
-    public ImageView image3;
-    public ImageView image1;
-    public ImageView image4;
-    public ImageView image5;
-    public Label description;
+    public ImageView factionImage2;
+    public ImageView factionImage3;
+    public ImageView factionImage1;
+    public ImageView factionImage4;
+    public ImageView factionImage5;
+    public Label factionDescription;
     @FXML
     private Pane changeFactionPane;
     public Pane mainPane;
-    public VBox box3;
-    public VBox box2;
-    public VBox box1;
     public FlowPane selectCardFlowPane;
-    private ArrayList<ImageView> images = new ArrayList<ImageView>();
+    private ArrayList<ImageView> factionImages = new ArrayList<ImageView>();
     private int[] tmp = new int[]{0, 1, 2, 3, 4};
     private HashMap<String, ImageView> factions = new HashMap<String, ImageView>();
     private ArrayList<String> factionNames = Game.getAllFactions();
+    private boolean changeFactionClicked = false;
+    private boolean changeLeadeClicked = false;
 
     public void initialize() {
         changeFactionPane.setVisible(false);
 
-        images.add(image1);
-        images.add(image2);
-        images.add(image3);
-        images.add(image4);
-        images.add(image5);
+        factionImages.add(factionImage1);
+        factionImages.add(factionImage2);
+        factionImages.add(factionImage3);
+        factionImages.add(factionImage4);
+        factionImages.add(factionImage5);
 
         for (String cardName : factionNames) {
             factions.put(cardName, new ImageView(new Image(Objects.requireNonNull(GameView.class.getResource("/Assets/Factions/faction_" + cardName + ".jpg")).toExternalForm())));
         }
 
         int counter = -1;
-        for (ImageView imageFaction : images) {
+        for (ImageView imageFaction : factionImages) {
             counter++;
-            if (imageFaction.equals(image3)) {
+            if (imageFaction.equals(factionImage3)) {
                 imageFaction.setImage(factions.get(Game.getLoggedInUser().getFaction()).getImage());
-                description.setText(Game.getLoggedInUser().getFaction());
+                factionDescription.setText(Game.getLoggedInUser().getFaction());
             } else if (!factionNames.equals(Game.getLoggedInUser().getFaction())) {
                 imageFaction.setImage(factions.get(factionNames.get(counter)).getImage());
             }
@@ -107,66 +96,52 @@ public class PreGameViewController {
     public void ChangeFaction(MouseEvent mouseEvent) {
 //        mainPane.setDisable(true);
         changeFactionPane.setVisible(true);
+        changeFactionClicked = true;
+        ;
     }
 
     @FXML
     private void handleBoxClick(MouseEvent event) {
         VBox selectedBox = (VBox) event.getSource();
-        updateBoxSizes(selectedBox);
     }
 
-    private void updateBoxSizes(VBox selectedBox) {
-        VBox[] boxes = {box1, box2, box3}; // Add more VBox elements as needed
-        for (VBox box : boxes) {
-            if (box == selectedBox) {
-                box.getStyleClass().add("selected");
-                box.getStyleClass().remove("smaller");
-            } else {
-                box.getStyleClass().add("smaller");
-                box.getStyleClass().remove("selected");
-            }
-        }
-    }
 
     public void changeFaction() {
 
     }
 
     public void moveRight(MouseEvent mouseEvent) {
-        int cnt = tmp[4];
-        for (int i = 4; i > 0; i--) {
-            tmp[i] = tmp[i - 1];
-        }
-        tmp[0] = cnt;
-
-        int counter = -1;
-        for (ImageView imageFaction : images) {
-            counter++;
-            imageFaction.setImage(factions.get(factionNames.get(tmp[counter])).getImage());
-        }
-
-        description.setText(factionNames.get(tmp[2]));
+        if (changeFactionClicked)
+            moveMethod(factionImages,factions,factionNames,factionDescription,0,4);
     }
 
     public void moveLeft(MouseEvent mouseEvent) {
-        int cnt = tmp[0];
-        for (int i = 0; i < 4; i++) {
-            tmp[i] = tmp[i + 1];
-        }
-        tmp[4] = cnt;
+        if (changeFactionClicked)
+            moveMethod(factionImages,factions,factionNames,factionDescription,4,0);
+    }
 
+    private void moveMethod(ArrayList<ImageView> images, HashMap<String, ImageView> cards, ArrayList<String> cardsName, Label description,int begin,int end) {
+        int cnt = tmp[end];
+        for (int i = end; i > begin; i--) {
+            tmp[i] = tmp[i - 1];
+        }
+        tmp[begin] = cnt;
 
         int counter = -1;
         for (ImageView imageFaction : images) {
             counter++;
-            imageFaction.setImage(factions.get(factionNames.get(tmp[counter])).getImage());
+            imageFaction.setImage(cards.get(cardsName.get(tmp[counter])).getImage());
         }
 
-        description.setText(factionNames.get(tmp[2]));
+        description.setText(cardsName.get(tmp[2]));
+        if(changeFactionClicked)
+            Game.getLoggedInUser().setFaction(factionNames.get(tmp[2]));
+
     }
 
     public void closeFactionPane(MouseEvent mouseEvent) {
         changeFactionPane.setVisible(false);
+        changeFactionClicked = false;
 //        mainPane.setDisable(false);
     }
 }
