2a2d3013697a1433ab1a7ccf4665acff2c72b54a
sarina <sarina.farzadnasab@gmail.com>
2024-07-04 13:26:56 +0330

    game history addded

--- src/main/java/views/GameHistory.java
+++ src/main/java/views/GameHistory.java
@@ -1,2 +1,24 @@
-package views;public class GameHistory {
+package views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import models.Game;
+
+import java.util.Objects;
+
+public class GameHistory extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Game.stage = stage;
+        Pane pane = FXMLLoader.load(Objects.requireNonNull(MainMenu.class.getResource("/FXML/GameHistoryFXML.fxml")));
+        stage.setScene(new Scene(pane));
+        stage.show();
+
+    }
 }
