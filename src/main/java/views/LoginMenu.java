94f1ccea12f30e5ffe61f58e8f2a1414f051d2bb
MahMosTash <MahdyarMostasharh@gmail.com>
2024-07-04 12:30:27 +0330

    MatchTable Connected to PreGameMenu

--- src/main/java/views/LoginMenu.java
+++ src/main/java/views/LoginMenu.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import models.Game;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -17,7 +18,7 @@ public class LoginMenu extends PlayMenu {
     public void start(Stage stage) throws Exception {
       //TODO
 //        Game.setAllUsers(Objects.requireNonNull(DataSaver.loadUsers()));
-        LoginMenu.stage = stage;
+        Game.stage = stage;
         Pane pane = FXMLLoader.load(Objects.requireNonNull(LoginMenu.class.getResource("/FXML/LoginMenuFXML.fxml")));
         stage.setScene(new Scene(pane));
         stage.show();
