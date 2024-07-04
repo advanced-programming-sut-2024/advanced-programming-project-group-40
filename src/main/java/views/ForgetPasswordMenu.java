b02cb571e5462083ab4244b8a78af8970c912681
MahMosTash <MahdyarMostasharh@gmail.com>
2024-07-04 11:12:21 +0330

    Bullshit Classes Deleted

--- src/main/java/views/ForgetPasswordMenu.java
+++ src/main/java/views/ForgetPasswordMenu.java
@@ -6,11 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class ForgetPasswordMenu extends PlayMenu {
     @Override
     public void start(Stage stage) throws Exception {
         GameView.stage = stage;
-        Pane pane = FXMLLoader.load(LoginMenu.class.getResource("/FXML/ForgetPasswordMenuFXML.fxml"));
+        Pane pane = FXMLLoader.load(Objects.requireNonNull(LoginMenu.class.getResource("/FXML/ForgetPasswordMenuFXML.fxml")));
         stage.setScene(new Scene(pane));
         stage.show();
     }
