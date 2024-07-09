3d8a6de1e0e8fd15d7418d2f786ab0fb1e2caaae
MahMosTash <MahdyarMostasharh@gmail.com>
2024-07-09 19:09:29 +0330

    Test Structure Added

---
+++ src/test/java/controllers/MenuController/LoginMenuControllerTest.java
@@ -0,0 +1,38 @@
+package controllers.MenuController;
+
+import Server.ClientHandler;
+import Server.Messages.Client.LoginMessages;
+import Server.Messages.ServerMessages;
+import models.Game;
+import models.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class LoginMenuControllerTest {
+
+    @Mock
+    private User user;
+
+    @Mock
+    private ClientHandler clientHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(Game.getUserByName(anyString())).thenReturn(user);
+        when(ClientHandler.client.login(any(LoginMessages.class))).thenReturn(new ServerMessages(true, ""));
+    }
+
+    @Test
+    void testStayLoggedInSelected() {
+        Game.setLoggedInUser(user);
+        LoginMenuController.stayLoggedInSelected();
+        verify(user, times(1)).setStayLoggedIn(true);
+    }
+
+}
