cfa09c17f0115dc487e194392ab6e9983a95caaa
MahMosTash <MahdyarMostasharh@gmail.com>
2024-07-11 15:14:46 +0330

    Bugs Fixed

--- src/main/java/views/GameView.java
+++ src/main/java/views/GameView.java
@@ -1,29 +1,13 @@
 package views;
 
 
-import Server.ClientHandler;
-import Server.Messages.Client.UpdateMessage;
-import Server.Messages.MessageSubType;
-import enums.cards.UnitCardInfo;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import models.Game;
-import models.MatchTable;
-import models.UserInputHandler.ClickCommand;
-import models.cards.Card;
-import models.cards.UnitCard;
 
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
 
 public class GameView extends PlayMenu{
