e21c173cc0329267dc99279f02de1ea56d571b6d
sarina <sarina.farzadnasab@gmail.com>
2024-07-11 12:14:39 +0330

    some methods added

--- src/main/java/views/ViewController/TelevisionViewController.java
+++ src/main/java/views/ViewController/TelevisionViewController.java
@@ -1,4 +1,36 @@
 package views.ViewController;
 
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import models.Game;
+import views.MainMenu;
+import views.TerminalView;
+
 public class TelevisionViewController {
+    public VBox player1;
+    public VBox player2;
+    public VBox watch;
+
+
+
+    public void initialize(){
+
+    }
+
+    public void openTerminal(MouseEvent mouseEvent) {
+        try {
+            new TerminalView().start(new Stage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void goToLoginMenu(MouseEvent mouseEvent) {
+        try {
+            new MainMenu().start(Game.stage);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
